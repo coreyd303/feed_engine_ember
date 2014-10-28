f9293ba9ae632d4cfbc195634d86ad58c491b123
Corey Davis <coreyd303@gmail.com>
2014-10-28 12:04:29 -0600

    first test green

--- tests/helpers/start-app.js
+++ tests/helpers/start-app.js
@@ -5,7 +5,6 @@ import config from '../../config/environment';
 
 export default function startApp(attrs) {
   var App;
-
   var attributes = Ember.merge({}, config.APP);
   attributes = Ember.merge(attributes, attrs); // use defaults, but you can override;
 
