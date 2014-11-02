91873ddffa82883a2429c7c9fd787f5eb4c85a0f
Alan Smith <a.bax.smith@gmail.com>
2014-11-02 14:30:55 -0700

    adds functioning groups/new form

---
+++ app/controllers/groups/new.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+
+
+export default Ember.ObjectController.extend({
+  actions: {
+    submitGroup: function() {
+      var group = this.get('model');
+      group.save();
+      this.transitionToRoute('groups.index');
+     }
+  }
+});
--- app/routes/groups/new.js
+++ app/routes/groups/new.js
@@ -1,14 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-    model: function() {return this.store.createRecord('group');},
-    actions: {
-        create: function() {
-            var self = this;
-            this.controller.get('model').save().then(
-                function() {
-                    self.transitionTo('group.index');
-                });
-        }
-    }
+  model: function() {return this.store.createRecord('group'); }
 });
