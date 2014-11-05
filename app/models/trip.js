b2cda007d51eacb42649064f263672f2ffe66fb0
Andy Mention <amention@gmail.com>
2014-11-05 15:25:55 -0700

    Done with Trip Create page

--- app/models/trip.js
+++ app/models/trip.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr('string'),
   description: DS.attr('string'),
-  group: DS.belongsTo('group')
+  location: DS:attr('string'),
+  date: DS:attr('date')
 });
