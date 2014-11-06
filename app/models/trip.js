c8e7b3a20c2e1e1e981ed83660180bbc90c7bb1d
Andy Mention <amention@gmail.com>
2014-11-06 09:36:09 -0700

    change 'location' to 'trip_location' because js

--- app/models/trip.js
+++ app/models/trip.js
@@ -3,7 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr('string'),
   description: DS.attr('string'),
-  location: DS.attr('string'),
+  trip_location: DS.attr('string'),
   date: DS.attr('date'),
   users: DS.hasMany('user')
 });
